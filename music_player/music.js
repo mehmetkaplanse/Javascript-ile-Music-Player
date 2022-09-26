2b1236f5db43c4fcbc7c9656ae6b23c66046f59b
Mehmet Kaplan <mhmt_kpln33@hotmail.com>
2022-09-26 16:22:02 +0300

    Müzik Çalar Programı
    Javascript ile geliştirmiş olduğum Müzik Çalar Programı

---
+++ music_player/music.js
@@ -0,0 +1,21 @@
+
+class Music {
+    constructor(title, singer, img, file) {
+        this.title = title;
+        this.singer = singer;
+        this.img = img;
+        this.file = file;
+    }
+
+    getName() {
+        return this.title + " - " + this.singer;
+    }
+
+}
+
+
+const musicList = [
+    new Music("İmdat","Çakal","1.jpeg","1.mp3"),
+    new Music("Bu da Geçer mi Sevgilim","Yalın","2.jpeg","2.mp3"),
+    new Music("Halden Anlamaz","Okan Volkan","3.jpeg","3.mp3")
+];
